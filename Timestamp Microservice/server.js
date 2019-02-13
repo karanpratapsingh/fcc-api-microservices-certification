2f63e505c8389708612afd85f3499f0b6946c82b
Karan Pratap Singh <synester.gates4@gmail.com>
2019-02-13 21:00:00 +0530

    added first project

---
+++ Timestamp Microservice/server.js
@@ -0,0 +1,62 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/timestamp/:date', (req, res) => {
+
+
+    let { date } = req.params;
+  
+   if (date !== undefined) {
+
+
+      let unixTimestamp = parseInt(date * 1);
+     
+      if (isNaN(unixTimestamp)) {
+        
+        date = new Date(date);
+      } else {
+        
+        date = new Date(unixTimestamp);
+      }
+      
+    } else {
+      
+      date = new Date(Date.now());
+    }
+    
+
+     let response = date == 'Invalid Date' ? { error: 'Invalid Date' } : { "unix": date.getTime(), "utc": date.toUTCString() };
+    
+     res.json(response);
+  });
+
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
