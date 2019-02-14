234262ef7b2ab3e9271d8d5a4819c35f6ac1fa57
Karan Pratap Singh <synester.gates4@gmail.com>
2019-02-14 21:19:13 +0530

    added new Project :boom:

---
+++ File Metadata Microservice/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
+let app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), ({ file:{ originalname, mimetype, size }}, res) => res.json({
+    "name" : originalname,
+    "type" : mimetype,
+    "size" : size
+}));
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 > Timestamp Microservice (https://sunset-adapter.glitch.me/)
 > Request Header Parser Microservice (https://dawn-crib.glitch.me/)
 > URL Shortener Microservice (https://silly-bat.glitch.me/)
+
+> File Metadata Microservice (https://cuddly-astronomy-1.glitch.me/)
